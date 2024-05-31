ff0d17bf60cbfcf9e3274c21c4d4ba4a325d2a3a
danielandersonBC96 <danielandersonbrandaocampos@hotmail.com>
2024-05-31 07:19:02 -0400

    correção

--- src/Models/accontmodels1.js
+++ src/Models/accontmodels1.js
@@ -1,23 +1,23 @@
-
 const mongoose = require('mongoose');
 
 const accountSchema = new mongoose.Schema({
     userId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'User',
-      required: true
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
     },
     accountNumber: {
-      type: String,
-      required: true,
-      unique: true
+        type: String,
+        required: true,
+        unique: true
     },
     balance: {
-      type: mongoose.Decimal128, // Corrigido para mongoose.Decimal128
-      default: 0.0
+        type: mongoose.Schema.Types.Decimal128, // Certifique-se de que está correto
+        default: 0.0
     }
 });
 
-const Account = mongoose.model('Account', accountSchema);
+// Verifica se o modelo já foi definido
+const Account = mongoose.models.Account || mongoose.model('Account', accountSchema);
 
 module.exports = Account;
